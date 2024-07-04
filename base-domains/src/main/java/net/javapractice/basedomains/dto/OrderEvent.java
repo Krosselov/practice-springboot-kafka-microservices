d98153d206e88cd813a800ad4f95c1d5744fd0fb
Vadim Shupan <shupan.vadimka@mail.ru>
2024-07-04 03:02:59 +0300

    init: initial commit

---
+++ base-domains/src/main/java/net/javapractice/basedomains/dto/OrderEvent.java
@@ -0,0 +1,16 @@
+package net.javapractice.basedomains.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderEvent {
+
+    private String message;
+    private String status;
+    private Order order;
+}
